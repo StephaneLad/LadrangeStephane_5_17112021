33e5b402527b919d35eaa8d947ca5b8292fed5d5
Stephane Ladrange <stephaneladrange@live.fr>
2021-12-12 11:22:38 +0100

    cartjs

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,13 +1,30 @@
 let i=0
 let product =JSON.parse(localStorage.getItem('product'))
-const cartItem = document.getElementById('cart__items')
-console.log(product)
-let s =product.lenght
+const cartItem = document.getElementById('cart__items') 
 
+let test = document.getElementById('order')
+let deleteItem
+let quantity
+let quantityShow
 
+let form =document.querySelector('.cart__order__form').getElementsByTagName('input')
+let error =document.querySelector('.cart__order__form').getElementsByTagName('p')
+
+// let firstName = document.getElementById('firstName')
+// let firstNameError = document.getElementById('firstNameErrorMsg')
+// let lastName = document.getElementById('lastName')
+// let lastNameError = document.getElementById('lastNameErrorMsg')
+// let adress = document.getElementById('address')
+// let adressError = document.getElementById('addressErrorMsg')
+// let city = document.getElementById('city')
+// let cityError = document.getElementById('cityErrorMsg')
+// let email = document.getElementById('email')
+// let emailError = document.getElementById('emailErrorMsg')
+// let submit = document.getElementById('order')
 
 // console.log(cartItem)
-while(i<s){
+
+while(i<product.length){
       cartItem.innerHTML += `
         <article class="cart__item" data-id="${product[i].id}" data-color="${product[i].color}">
         <div class="cart__item__img">
@@ -21,7 +38,7 @@ while(i<s){
           </div>
           <div class="cart__item__content__settings">
             <div class="cart__item__content__settings__quantity">
-              <p>Qté : ${product[i].quantity}</p>
+              <p class="cart__item__content__settings__quantity_show">Qté : ${product[i].quantity}</p>
               <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product[i].quantity}">
             </div>
             <div class="cart__item__content__settings__delete">
@@ -32,6 +49,37 @@ while(i<s){
       </article>
         `
   i +=1
-  console.log('test')
+
+  deleteItem = document.querySelectorAll('.cart__item__content__settings__delete')
+  quantity = document.querySelectorAll('.itemQuantity')
+  quantityShow = document.querySelectorAll('.cart__item__content__settings__quantity_show')
+}
+test.addEventListener('click',()=>{console.log('test')})
+
+for (let y = 0; y < deleteItem.length; y++) {
+  deleteItem[y].addEventListener("click", () => {
+    product.splice(y,1)
+    localStorage.setItem('product',JSON.stringify(product))
+    window.location.reload()
+  }); 
+}
+
+for (let y = 0; y < quantity.length; y++) {
+  quantity[y].addEventListener('change', () =>{
+    product[y].quantity=parseInt(quantity[y].value)
+    localStorage.setItem('product',JSON.stringify(product))
+    quantityShow[y].innerHTML=`Qté : ${quantity[y].value}`
+  })
+  
+}
+
+
+for (let y = 0; y < form.length; y++) {
+  if(!form[y].value){
+    error[y].innerHTML=`veuillez renseignez votre ${form[y].name}`
+  }else{
+    console.log(y)
+  }
+  
   
 }
--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,5 @@
 let urlId = window.location.search 
 let id = urlId.slice(4)
-
-let z =0
 let priceItem
 let names
 let imgItem
@@ -14,7 +12,7 @@ const colorChoice = document.getElementById('colors')
 const addCart = document.getElementById('addToCart')
 console.log(addCart)
 
-async function tutu (){
+async function productItem (){
     await fetch (`http://localhost:3000/api/products/${id}`)
     .then(res=>res.json())
     .then(data=>{
@@ -35,27 +33,28 @@ async function tutu (){
     })
 ;
 }
-tutu().then(_ => console.log(names) )
+productItem().then(_ => console.log(names) )
 
 addCart.addEventListener('click', () =>{   
-    let quantity = document.getElementById('quantity').value
+    let quantity = parseInt(document.getElementById('quantity').value)
     let color = document.getElementById('colors').value
     let product =JSON.parse(localStorage.getItem('product'))
     let i =0
+    let newProduct = false
     if (product === null){
         product = [{id:id,color:color,quantity:quantity,nameItem:names,price:priceItem,img:imgItem}]
     }else{
         while(i<product.length){
             if(id===product[i].id & color===product[i].color){
-              parseInt(product[i].quantity) += parseInt(quantity)
-            }else{
-                if(i===product.length){
-                    product= [...product,{id:id,color:color,quantity:quantity,nameItem:names,price:priceItem,img:imgItem}]
-                }
+              product[i].quantity += quantity
+              newProduct = true
             }
             i+=1
         }
-        
+        if(i===product.length & !newProduct){
+            product= [...product,{id:id,color:color,quantity:quantity,nameItem:names,price:priceItem,img:imgItem}]
+            console.log('test')
+        }
     }
     product = JSON.stringify(product)
     localStorage.setItem('product',product)
@@ -63,72 +62,5 @@ addCart.addEventListener('click', () =>{
 
 
 
-    // let productItem = [product,{id:id,color:color,quantity:quantity,nameItem:names,price:priceItem,img:imgItem}]
-    // localStorage.setItem('test',JSON.stringify(productItem))
-    // console.log(localStorage.getItem('test'))
-    // let u = JSON.parse(localStorage.getItem('test'))
-    // console.log(u)
-
-    
-    
-
-    
-    
-    // let i =0
-    // if (productItem === null){
-    //     productItem= {id:id,color:color,quantity:quantity,nameItem:names,price:priceItem,img:imgItem}
-    // }
-    // while (i===productItem.length){
-    //     let idStore = productItem[i].id
-    //     let colorStore = productItem[i].color
-    //     if(id===idStore & color===colorStore){
-    //         productItem[i].quantity += quantity
-    //     }else{
-    //         if(i === productItem.length){
-    //             productList = {id:id,color:color,quantity:quantity,nameItem:names,price:priceItem,img:imgItem}
-    //         }
-    //     }
-    //     i+=1
-    // }
-    // // let list = JSON.stringify([...color,productItem,productList])
-    // let test = [productItem,productList]
-    // console.log(test)
-    // localStorage.setItem('product',list)
-    
 
 })
-// let test = {id:id,te:'tetstset'}
-// localStorage.setItem('ty',JSON.stringify(test))
-// let hg = JSON.parse(localStorage.getItem('ty'))
-// console.log(hg)
-// let df = JSON.parse(hg)
-// console.log(df)
-
-// console.log('teststeestsetsetestst')
-// let ty = JSON.stringify(test)
-// let yu =JSON.parse(ty)
-// console.log(test)
-// console.log(ty)
-// console.log(yu)
-// let test = [{id:12457,color:'red',quantity:12},{id:4520,color:'lol',quantity:14}]
-// let i =0
-// console.log(test)
-// let u = {id:12457,color:'red',quantity:10}
-// let t = {id:12457,color:'ye',quantity:10}
-// while (i< test.length){
-//     if(u.id===test[i].id & u.color===test[i].color){
-//         test[i].quantity += u.quantity
-//     }else{
-//         console.log('sasaas')
-//     }
-//     i+=1
-// }
-// console.log(test)
-
-
-// let i =0
-// let test = 10
-// while (i<=test){
-//     i+=1
-//     console.log(i)
-// }
